d2cee915aa5d75781a7bac5f74271c07c499a93a
brenosekiro <b88660501@gmail.com>
2020-06-20 14:48:49 -0300

    Home

---
+++ mobile/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './screens/Home'
+import { StyleSheet, View, Text, ImageBackground, Dimensions } from 'react-native'
+import 'react-native-gesture-handler'
+
+const App = () => {
+    return (
+        <Home />
+    )
+}
+
+
+
+export default App
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { StyleSheet, View, Text, ImageBackground, Dimensions } from 'react-native'
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { RectButton } from 'react-native-gesture-handler'
+
+const Home = () => {
+
+    const [fontsLoaded] = useFonts({ Roboto_400Regular })
+
+    if(!fontsLoaded) {
+        return <Text>Loading</Text>
+    }
+
+    return (
+        <View>
+           <ImageBackground style={styles.image} source={require('../../../assets/background.jpg')}>
+               <Text style={styles.mainText}>rest.inn</Text>
+               <Text style={styles.descText}>The best solution for rest during travels!</Text>
+               <View style={styles.form}>
+                    <View style={styles.pickers}>
+                        <View style={styles.picker}>
+                            <Text>Fortaleza</Text>
+                        </View>
+                        <View style={styles.picker}>
+                            <Text>CE</Text>
+                        </View>
+                    </View>
+                    <RectButton style={styles.button}>
+                        <Text style={styles.buttonText}>See inns</Text>
+                    </RectButton>
+                </View>
+           </ImageBackground>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: Dimensions.get('screen').width,
+        flex: 1,
+        alignItems: 'center'
+    },
+    mainText: {
+        fontFamily: 'Roboto_400Regular',
+        color: 'white',
+        fontSize: 70,
+        textAlign: 'center',
+        paddingTop: 30
+    },
+    descText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 15
+    },
+    picker: {
+        width: 150,
+        padding: 10,
+        backgroundColor: 'white',
+        margin: 5,
+        borderRadius: 2
+    },
+    pickers: {
+        width: 320,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginBottom: 10
+    },
+    form: {
+        position: 'absolute',
+        bottom: 50,
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: '#2133d9',
+        padding: 10,
+        borderRadius: 2,
+        width: 310,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color:  'white'
+    }
+})
+
+export default Home

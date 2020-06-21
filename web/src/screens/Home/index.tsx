65feddf58390da1c0eb0b1c73c0c4b4e3d63465e
brenosekiro <b88660501@gmail.com>
2020-06-21 19:22:24 -0300

    info

--- web/src/screens/Home/index.tsx
+++ web/src/screens/Home/index.tsx
@@ -3,11 +3,47 @@ import './styles.css'
 
 const Home = () => {
     return (
-        <div className="home-presentation">
-            <h2>rest.inn</h2>
-            <p>The best solution for rest during travels!</p>
-            <button>Register an Inn!</button>
-        </div>
+        <>
+            <div className="home-presentation">
+                <h2>rest.inn</h2>
+                <p>The best solution for rest during travels!</p>
+                <button>Register an Inn!</button>
+            </div>
+            <div className="home-info">
+                <div className="home-info-item">
+                    <div className="home-info-image">
+                        <img src="/happy.png" alt="easy"/>
+                    </div>
+                    <div className="home-info-description">
+                        <h3>Easy to use</h3>
+                        <p>Our interface was made in order to bring to the user a better experience while searching for good places to rest</p>
+                    </div>
+                </div>
+                <div className="home-info-item">
+                    <div className="home-info-image">
+                        <img src="/imobiliaria.png" alt="easy"/>
+                    </div>
+                    <div className="home-info-description">
+                        <h3>Easy to find</h3>
+                        <p>Our system sorts the places by state and city to smooth your search.</p>
+                    </div>
+                </div>
+                <div className="home-info-item">
+                    <div className="home-info-image">
+                        <img src="/rota.png" alt="easy"/>
+                    </div>
+                    <div className="home-info-description">
+                        <h3>High accuracy location</h3>
+                        <p>With the gps, you can find easily the place where you want to go.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <p>Icons made by <a href="https://www.flaticon.com/br/autores/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a></p>
+                <p>Icons made by <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a></p>
+                <p>Icons made by <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a></p>
+            </div>
+        </>
     )
 }
 

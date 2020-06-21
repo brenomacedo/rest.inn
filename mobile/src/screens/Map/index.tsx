ae6251435e102b94e0be8b37b35b22c6d985989b
brenosekiro <b88660501@gmail.com>
2020-06-20 22:36:11 -0300

    mapLocation

--- mobile/src/screens/Map/index.tsx
+++ mobile/src/screens/Map/index.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import MapView from 'react-native-maps'
+import { getCurrentPositionAsync } from 'expo-location'
 
 const MapScreen = () => {
+
+    interface IPosition {
+        lat: number
+        lng: number
+    }
+
+    const [position, setPosition] = useState<IPosition>({ lat: 0, lng: 0 })
+
+    useEffect(() => {
+        getCurrentPositionAsync().then(position => {
+            setPosition({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+        }).catch()        
+    }, [])
+
+
     return (
         <View style={styles.container}>
             <View style={styles.goBack}>
@@ -17,13 +37,28 @@ const MapScreen = () => {
                 </Text>
             </View>
             <View style={styles.map}>
-                
+            <MapView showsUserLocation style={styles.mapView}
+                initialRegion={{
+                latitude: 0,
+                longitude: 0,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }} region={{
+                latitude: position.lat,
+                longitude: position.lng,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}
+            />
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    mapView: {
+        flex: 1
+    },
     container: {
         flex: 1,
         backgroundColor: '#e6e8ff'

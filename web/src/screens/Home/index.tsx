b5f0991f7087b90c6cdfe09407148812dceea067
brenosekiro <b88660501@gmail.com>
2020-06-21 17:44:20 -0300

    home-presentation

---
+++ web/src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+const Home = () => {
+    return (
+        <div className="home-presentation">
+            <h2>rest.inn</h2>
+            <p>The best solution for rest during travels!</p>
+            <button>Register an Inn!</button>
+        </div>
+    )
+}
+
+export default Home

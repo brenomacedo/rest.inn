8cde6bda560c46e090f7b14dfdf59d82422b0349
brenosekiro <b88660501@gmail.com>
2020-06-23 09:23:21 -0300

    createInn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Home from './screens/Home'
 import Routes from './routes'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import './App.css'
 
 function App() {
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:3333'
+  })
+
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Routes />
+      </div>
+    </ApolloProvider>
   )
 }
 
--- web/src/screens/Register/index.tsx
+++ web/src/screens/Register/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { useMutation } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 import axios from 'axios'
 import './styles.css'
-import { env } from 'process'
+import { useHistory } from 'react-router-dom'
 
 const Register = () => {
 
@@ -23,15 +25,40 @@ const Register = () => {
             }).catch(err => console.log(err))
     }, [])
 
+    const history = useHistory()
+
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
     const [markerLocation, setMarkerLocation] = useState<[number, number]>([0, 0])
     const [states, setStates] = useState<State[]>([])
-    const [selectedState, setSelectedState] = useState<string>('')
+    const [selectedState, setSelectedState] = useState<string>('0')
     const [cities, setCities] = useState<City[]>([])
-    const [selectedCity, setSelectedCity] = useState<string>('')
+    const [selectedCity, setSelectedCity] = useState<string>('0')
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault()
+        if(name === '') {
+            alert('Selecione um nome!')
+            return
+        }
 
+        if(description === '') {
+            alert('Selecione uma descrição!')
+            return
+        }
+
+        if(markerLocation[0] === 0 || markerLocation[1] === 0) {
+            alert('Selecione uma localização!')
+            return
+        }
+
+        if(selectedCity === '0' || selectedState === '0') {
+            alert('Selecione uma cidade e um estado!')
+            return
+        }
+
+        createInn()
+        history.push('/success')
     }
 
     const changedState = (event: ChangeEvent<HTMLSelectElement>) => {
@@ -45,22 +72,49 @@ const Register = () => {
         setSelectedCity(event.target.value)
     }
 
+    const CREATE_INN = gql`
+        mutation CreateInn($name: String!, $description: String!, $lat: Float!, $lng: Float!, $state: String!, $city: String!) {
+            createInn(name: $name, description: $description, lat: $lat, lng: $lng, state: $state, city: $city) {
+                id
+                name
+                description
+                lat
+                lng
+                state
+                city
+            }
+        }
+    `
+
+    const [createInn] = useMutation(CREATE_INN, {
+        variables: {
+            name,
+            description,
+            lat: markerLocation[0],
+            lng: markerLocation[1],
+            state: selectedState,
+            city: selectedCity
+        }
+    })
+
     return (
         <div className="register">
             <div className="container">
                 <h2>Welcome!</h2>
                 <p>Here you can register you inn, remember yourself to put the city and the state correctly for the place appear for the users in that place.</p>
                 <form className="form">
-                    <input className="register-text" type="text" placeholder="name"/>
-                    <textarea className="register-textarea" placeholder="description"></textarea>
+                    <input value={name} onChange={e => setName(e.target.value)}
+                    className="register-text" type="text" placeholder="name"/>
+                    <textarea value={description} onChange={e => setDescription(e.target.value)}
+                    className="register-textarea" placeholder="description"></textarea>
                     <select defaultValue='0' onChange={changedState} value={selectedState} className="register-select">
-                        <option disabled value="0">Select your state</option>
+                        <option value="0">Select your state</option>
                         {states.map(state => (
                             <option key={state.id} value={state.sigla}>{state.sigla}</option>
                         ))}
                     </select>
                     <select onChange={changedCity} value={selectedCity} defaultValue='0' className="register-select">
-                        <option disabled value="0">Select your city</option>
+                        <option value="0">Select your city</option>
                         {cities.map(city => (
                             <option key={city.id} value={city.nome}>{city.nome}</option>
                         ))}
